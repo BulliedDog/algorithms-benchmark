5b31839aca0614e21a48c8dd1f81c8895a009a8f
BulliedDog <albertorovai1@gmail.com>
2025-10-12 12:17:59 +0200

    added heap class and node class

--- data structures/heap.py
+++ data structures/heap.py
@@ -1,5 +1,19 @@
 class heap:
     def __init__(self):
-        print("hello")
-    def sort(self):
-        return self
+        self.root = None # radice dell'albero, se è max-heap -> elemento massimo, se è min-heap -> elemento minimo
+        self.type = None # a seconda del valore di self.type, si comporterà come un max-heap o un min-heap
+    def heapify(self):
+
+    def insert(self, nodo): 
+        
+    def get(self): # only returns root, whether max or min
+        return self.root
+    def extract(self, index): #extracts the max or min depending on the self.type, then hipifies 
+
+class node:
+    def __init__(self,value):
+        self.value = value
+        self.index = None
+        self.parent = None
+        self.left = None
+        self.right = None
