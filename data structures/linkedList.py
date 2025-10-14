4aef1b1f6c99562088f574e0c16e038501aa25b1
BulliedDog <albertorovai1@gmail.com>
2025-10-14 09:35:31 +0200

    added more functions to the linkedList

--- data structures/linkedList.py
+++ data structures/linkedList.py
@@ -1,21 +1,37 @@
 class linkedList:
-    def __init__(self,):
+    def __init__(self, type):
         self.root = None
         self.size = 0
-    
+        self.type = type
+        
     def insert(self, node):
         if self.size == 0:
             self.root = node
-            self.size += 1
         else:
             lastNode = self._getLast()
-
+            lastNode.next = node
+        self.size += 1
+    
     def _getLast(self):
-        if self.root is None:
+        if self.size == 0:
             return None
-        else:
-            
-        
+        lastNode = self.root
+        while lastNode.next is not None:
+            lastNode = lastNode.next
+        return lastNode
+    
+    def extract(self): # estrae il massimo o il minimo
+        if self.size == 0:
+            return None
+        targetNode = self.root # nodo di appoggio max o min
+        node = targetNode.next
+        while node is not None:
+            if (node.value > targetNode.value if self.type == "max" else node.value < targetNode.value):
+                targetNode = node
+        return node
+    
+    
+
 class node:
     def __init__(self, value):
         self.value = value
